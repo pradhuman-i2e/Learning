c481912cd1527352a41810e2ab38d8fcbb02620c
pradhuman-i2e <pradhuman.chaudhary@i2econsulting.com>
2024-12-10 18:14:53 +0530

    Updated File and Checked

--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c64cd5d014799e7f8b8f980b06118efd9ef287")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adcd637a7cd94885af7c665dfc4df9331d654e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
